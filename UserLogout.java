60b4d1b978f5b888d676216dbab0d0fc00732b05
Javeed Shariff <96673407+shariffjaveed@users.noreply.github.com>
2022-01-19 14:47:16 +0530

    Add files via upload

---
+++ UserLogout.java
@@ -0,0 +1,21 @@
+package week2.day2;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class UserLogout {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://acme-test.uipath.com/login");
+		driver.findElement(By.id("email")).sendKeys("kumar.testleaf@gmail.com");
+		driver.findElement(By.id("password")).sendKeys("leaf@12");
+		driver.findElement(By.xpath("//button[@class='btn btn-primary']")).click();
+		System.out.println("The Page title is "+driver.getTitle());
+		driver.findElement(By.xpath("//form[@id='logout-form']/following::a[text()='Log Out']")).click();
+		driver.close();
+
+}
+}
